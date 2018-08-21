7e1905cc286c63d160da0e9c3a3b54be4fe13fab
safirelauene <safire.lauene@gmail.com>
2018-08-21 15:32:23 -0300

    Fix path to support emojione 3.0.0+

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/emojione/assets/css/emojione.css');
+    app.import(app.bowerDirectory + '/emojione/extras/css/emojione.css');
     app.import(app.bowerDirectory + '/emojione/lib/js/emojione.js');
     app.import('vendor/emojione.js', {
       exports: {
